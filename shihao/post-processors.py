5877bc26bbf9a57eed48e4dd20cad65073476722
Jinitaimei <zhangshihao2016@gmail.com>
2025-04-30 17:02:52 +0800

    stuff

--- shihao/post-processors.py
+++ shihao/post-processors.py
@@ -1,13 +1,20 @@
 import cv2
 from client import Client, PostProcessors
 
+import time
+
 client = Client()
 
 _post_processors = [PostProcessors.Google_IMDA]
 
 while True:
-    cv2.imshow(
-        'black',
-        client.get_frame(*_post_processors)[0]
-    )
-    cv2.waitKey(1)
+    start = time.time()
+    for i in range(0, 10):
+        cv2.imshow(
+            'black',
+            client.get_frame(PostProcessors.ContourBlue)[0]
+        )
+        cv2.waitKey(1)
+
+    frame = client.get_frame()[0]
+    print(f'[FPS] {10/(time.time()-start)}fps')
